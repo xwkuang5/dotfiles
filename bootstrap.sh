201b17bfbafee1314a97dbf122251ad62c0b1977
xwkuang5 <louisconfussing@gmail.com>
2020-06-04 22:17:44 -0400

    Remove unused files

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,37 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin master;
+# git pull origin master;
+
+function vim() {
+	read -p "Install vim configuration? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		echo "Installing vim configuration..."
+		# Add .vim to runtime path
+		echo "set rtp+=\$HOME/.vim" > $HOME/.vimrc
+		echo "source \$HOME/.vim/.vimrc" >> $HOME/.vimrc
+		echo "Done..."
+	fi;
+}
+
+function emacs() {
+	read -p "Install emacs configuration? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		echo "Installing emacs configuration..."
+		# Chemacs for easy profile change
+		git clone https://github.com/plexus/chemacs.git ~/chemacs
+		~/chemacs/install.sh
+
+		# Install spacemacs
+		git clone https://github.com/syl20bnr/spacemacs ~/spacemacs
+		pushd ~/spacemacs && git checkout develop && popd
+
+		echo "Done..."
+		echo "Use emacs --with-profile <profile> to launch emacs (default: spacemacs)"
+	fi;
+}
 
 function doIt() {
 	rsync --exclude ".git/" \
@@ -11,8 +41,13 @@ function doIt() {
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
+		--exclude "settings.json" \
+		--exclude "brew.sh" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
+
+	vim;
+	emacs;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
