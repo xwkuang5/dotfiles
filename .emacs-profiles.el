8ff6773bc1f35f35fe651da9e304511bfae21131
xwkuang5 <louisconfussing@gmail.com>
2021-01-30 22:47:00 -0500

    Update bootstrap sript to install my own emacs configuration and set as default

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,3 +1,2 @@
 (("default"      . ((user-emacs-directory . "~/emacs.d")))
- ("doom"         . ((user-emacs-directory . "~/doom-emacs")))
  ("spacemacs"    . ((user-emacs-directory . "~/spacemacs"))))
